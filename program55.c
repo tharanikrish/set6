7b6c95c7d344d2c1ccbed65d17ba8306798f4724
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-03-02 16:51:10 +0530

    Create program55.c

---
+++ program55.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,m,mul;
+printf("\nenter the two numbers:);
+scanf("%d%d",&a,&b);
+mul=n*m;
+if(mul%2==0)
+{
+printf("\neven");
+}
+else
+printf("\nodd");
+return 0;
+}
