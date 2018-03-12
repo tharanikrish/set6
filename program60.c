12583e0feaf2fc8429231b86d959fb9177602474
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-03-12 16:33:29 +0530

    Create program60.c

---
+++ program60.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,t1=0,t2=1;i,temp;
+printf("\nenter the number:");
+scanf("%d",&n);
+printf("\nfibonacci series\n");
+for(i=0;i<n;i++)
+{
+printf("\n%d",t1);
+temp=t1+t2;
+t1=t2;
+t2=temp;
+}
+return 0;
+}
