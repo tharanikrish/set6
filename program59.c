74c2cbd5628a1ab9a5b6d4c1a0e5e4845389fbff
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-03-12 16:15:08 +0530

    Create program59.c

---
+++ program59.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,temp;
+printf("\nenter 10 numbers:");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+temp=a[0];
+for(i=0;i<10;i++)
+{
+if(a[i]>temp)
+{
+printf("\n%d",a[i]);
+}
+}
+return 0;
+}
