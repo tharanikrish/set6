0ba7e7408aafd8163c4f7c3ef6935cab714ef7ad
tharanikrish <31505996+tharanikrish@users.noreply.github.com>
2018-03-12 15:53:00 +0530

    Create program56.c

---
+++ program56.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+char a[10];
+int i,flag1=0,flag2=0;
+printf("\nenter the string:");
+scanf("%s",&a);
+for(i=0;a[i]!='\0';++i)
+{
+if(((a[i]>=65)&&(a[i]<=90))||((a[i]>=97)&&(a[i]<=122)))
+{
+flag1=1;
+}
+else if(a[i]>=48)&&(a[i]<=57))
+{
+flag2=1;
+}
+}
+if((flag1==1)&&(flag2==1))
+{
+printf("\nthe string contain both alphabet and number.");
+}
+return 0;
+}
